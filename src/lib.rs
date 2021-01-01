19873514104de5909f3bf08a773cdcbeec105e52
Inseok Lee <dlunch@gmail.com>
2021-01-01 15:17:29 +0900

    swc parser test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use std::str;
 
 use proc_macro::TokenStream;
 use quote::quote;
+use swc_common::BytePos;
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
 use syn::{parse_macro_input, LitStr};
 
 #[proc_macro]
@@ -13,7 +15,17 @@ pub fn typescript(input: TokenStream) -> TokenStream {
     let file = fs::read(file_path).unwrap();
 
     let content = str::from_utf8(&file).unwrap();
-    eprintln!("{}", content);
+    let lexer = Lexer::new(
+        Syntax::Typescript(Default::default()),
+        Default::default(),
+        StringInput::new(content, BytePos(0), BytePos(content.len() as u32)),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+    let module = parser.parse_typescript_module().unwrap();
+
+    eprintln!("{:?}", module.body[0]);
 
     (quote! {
         "TEST"
