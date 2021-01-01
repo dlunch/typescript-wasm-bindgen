88b02d291f33a377ce17bd42f32a0ba61774168d
Inseok Lee <dlunch@gmail.com>
2021-01-02 08:46:40 +0900

    Use parse_macro_input

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 mod codegen;
 
 use proc_macro::TokenStream;
-use syn::{parse::Parser, punctuated::Punctuated, LitStr, Token};
+use syn::{parse_macro_input, punctuated::Punctuated, LitStr, Token};
 
 #[proc_macro]
 pub fn typescript(input: TokenStream) -> TokenStream {
-    let parser = Punctuated::<LitStr, Token![,]>::parse_separated_nonempty;
-    let args = parser.parse(input).unwrap();
-    let (filename, module_name) = (&args[0], &args[1]);
+    let input = parse_macro_input!(input with Punctuated::<LitStr, Token![,]>::parse_separated_nonempty);
+    let (filename, module_name) = (&input[0], &input[1]);
 
     codegen::generate_wasm_bindgen_bindings(&filename.value(), &module_name.value()).into()
 }
